7201e58df9bf6d40f52a32d92d0ce0854186baba
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-06-10 18:55:46 +0700

    complit taskthree

--- HomeWorkOne/TaskThree/Program.cs
+++ HomeWorkOne/TaskThree/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача№ 3:
+ Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+try
+{
+    Console.WriteLine("Программа по проверки числа на чётноесть.\n");
+    Console.Write("Введетие число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int resalt = number % 2;
+    if (resalt == 0)
+    {
+        Console.Write("Число чётное!");
+    }
+    else
+    {
+        Console.Write("Число не чётное!");
+    }
+}
+catch
+{
+    Console.Write("Введены не корректные символы.");
+}
+
