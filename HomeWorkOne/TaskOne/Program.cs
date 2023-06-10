705d3320b25539237866d6c7217fee24e8156d38
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-06-10 21:40:27 +0700

    Опробовал и описал новую  конструкцию. Работает.

--- HomeWorkOne/TaskOne/Program.cs
+++ HomeWorkOne/TaskOne/Program.cs
@@ -4,27 +4,35 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
-try
-{
-    Console.Write("Введите первое число для сравнения: ");
-    int numberOne = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите второе число для сравнения: ");
-    int numberTwo = Convert.ToInt32(Console.ReadLine());
 
-    if (numberOne > numberTwo)
-    {
-        Console.Write($"Число {numberOne} больше, чем число {numberTwo}");
-    }
-    else if (numberOne < numberTwo)
-    {
-        Console.Write($"Число {numberTwo} больше, чем число {numberOne}");
-    }
-    else
-    {
-        Console.Write("Числа равны.");
-    }
-}
-catch
+Console.Write("Введите первое число для сравнения: ");
+//Прменил предложенный преподователем вариант.
+//По сути, конструкция: int.TryParse(<сюда ложиться переменная или воодятся данные>, out int <преобразованная в число строка>);
+// позволяет преобразовать строку в число, и проверить успешность этого преобразования. 
+//При этом, false  обрабатывается как исключение и машина не падает, что позволяет не использовать try/catch. 
+// Получаемое true или false можно присовить в булевую переменную и использовать.
+// Если преобразование успешно, то можно использовать конечную переменную дальше. 
+bool numOneTrue = int.TryParse(Console.ReadLine(), out int numberOne);
+Console.Write("Введите второе число для сравнения: ");
+bool numTwoTrue = int.TryParse(Console.ReadLine(), out int numberTwo);
+if (numOneTrue == false || numTwoTrue == false)
 {
     Console.Write("Введены не корректные символы.");
 }
+else if (numberOne > numberTwo)
+{
+    Console.Write($"Число {numberOne} больше, чем число {numberTwo}");
+}
+else if (numberOne < numberTwo)
+{
+    Console.Write($"Число {numberTwo} больше, чем число {numberOne}");
+}
+else
+{
+    Console.Write("Числа равны.");
+}
+
+
+
+
+
