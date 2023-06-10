8d123a5a506413f1a78375cfa2a2face2c604947
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-06-10 18:43:03 +0700

    complit tasktwo

--- HomeWorkOne/TaskTwo/Program.cs
+++ HomeWorkOne/TaskTwo/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/* Задача №2:
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+ХАРДКОД*/
+try
+{
+    Console.WriteLine($"Программа для поиска максимального числа.\n");
+    Console.Write("Введите первое число: ");
+    int numOne = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    int numTwo = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите третье число: ");
+    int numThree = Convert.ToInt32(Console.ReadLine());
+
+    if (numOne > numTwo)
+    {
+        if (numOne > numThree)
+        {
+            Console.Write($"Первое число {numOne} максимальное из трёх.");
+
+        }
+        else if (numOne == numThree)
+        {
+
+            Console.Write($"Первое число {numOne} и третье число {numThree} равны и больше числа {numTwo}");
+        }
+
+        else
+        {
+            Console.Write($"Третье число {numThree} максимальное из трёх.");
+        }
+    }
+    else if (numOne == numTwo)
+    {
+        if (numOne > numThree)
+        {
+            Console.Write($"Превое число {numOne} и втореое число {numTwo} равны и больше числа {numThree}");
+        }
+        else
+        {
+            Console.Write("Все числа равны.");
+        }
+    }
+    else if (numTwo > numThree)
+    {
+        Console.Write($"Второе число {numTwo} максимальное из трёх.");
+    }
+    else if (numTwo == numThree)
+    {
+        Console.Write($"Второе число {numTwo} и третье число {numThree} равны и больше числа {numOne}");
+    }
+    else
+    {
+        Console.Write($"Третье число {numThree} максимальное из трёх.");
+    }
+}
+catch
+{
+    Console.Write("Введены не корректные символы.");
+}
