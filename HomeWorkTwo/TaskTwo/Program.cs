31bacec3a25546263d6ebf6315b34ce2854d83ff
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-06-14 23:45:31 +0700

    complit TaskTwo

--- HomeWorkTwo/TaskTwo/Program.cs
+++ HomeWorkTwo/TaskTwo/Program.cs
@@ -5,4 +5,35 @@
     78 -> третьей цифры нет
     32679 -> 6
 */
-Console.WriteLine("Hello, World!");
+//Не вижу смысла делать два варианта как в прошлом Task. Разницы, по сути не будет. 
+//Так же не вижу смысла всё сваливать в один метод.
+void ImputAndExamMumber()
+{
+    Console.Write("Введите число: ");
+    bool trueNumber = int.TryParse(Console.ReadLine(), out int number);
+    GetMeThreeNumber(number, trueNumber);
+}
+
+void GetMeThreeNumber(int number, bool trueNumber)
+{
+    if (number > 99 && trueNumber == true)
+    {
+        for (int i = 1000; i < number;)
+        {
+            number = number / 10;
+        }
+        number = number % 10;
+        Console.WriteLine(number);
+    }
+    else if (trueNumber == true)
+    {
+        Console.WriteLine("Число меньше трёх.");
+    }
+    else
+    {
+        Console.WriteLine("Введены не корректные символы.");
+    }
+}
+ImputAndExamMumber();
+
+//Попробовать через List, массив и строки.
