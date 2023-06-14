461c28f85dad9b341a2e46bb9d548ebaddb58f09
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-06-15 00:03:49 +0700

    complit TaskThree

--- HomeWorkTwo/TaskThree/Program.cs
+++ HomeWorkTwo/TaskThree/Program.cs
@@ -5,4 +5,29 @@
     7 -> да
     1 -> нет
 */
-Console.WriteLine("");
+//
+void GetMEDayOff()
+{
+    Console.WriteLine("Введите номер дня недели: ");
+    int.TryParse(Console.ReadLine(), out int numDay);
+
+    switch (numDay)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            Console.Write("Не выходной. ):");
+            break;
+        case 6:
+        case 7:
+            Console.Write("ВЫХОДНОЙ!");
+            break;
+        default:
+            Console.Write("Вас просили ввести цифру соответсвующую дню недели.");
+            break;
+    }
+}
+GetMEDayOff();
+
