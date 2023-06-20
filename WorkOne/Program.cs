b502759d063c408cfe69a818320c82f9d65f53d1
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-06-21 01:40:13 +0700

    Фишки. Для массива и получения длинны числа.

--- WorkOne/Program.cs
+++ WorkOne/Program.cs
@@ -1,34 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Введите номер дня недели: ");
-int.TryParse(Console.ReadLine(), out int numDay);
-Day day = (Day)numDay;
-
-switch (day)
-{
-    case Day.Понедельник:
-    case Day.Вторник:
-    case Day.Среда:
-    case Day.Четверг:
-    case Day.Пятница:
-        Console.Write($"{day}, Не выходной. ):");
-        break;
-    case Day.Суббота:
-    case Day.Воскресенье:
-        Console.Write($"{day}, ВЫХОДНОЙ!");
-        break;
-    default:
-        Console.Write("Вас просили ввести цифру соответсвующую дню недели.");
-        break;
-}
-
-enum Day
-{
-    Понедельник = 1,
-    Вторник = 2,
-    Среда = 3,
-    Четверг = 4,
-    Пятница = 5,
-    Суббота = 6,
-    Воскресенье = 7
-}
+int[] array = { 1, 2, 3 };
+Console.Write(string.Join(" ", array));//Быстрый способ распечатать массив.
+int num = 1246;
+int length = (int)Math.Log10(num) + 1; //Быстрый способ получить длину числа. Через логорифм.
+Console.WriteLine();
+Console.Write(length);
