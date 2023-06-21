5d919c7dd8e25be2f6a3c2bc128424fafb9f216f
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-06-21 19:13:11 +0700

    Complit TaskTwo

--- HomeWorkFour/TaskTwo/Program.cs
+++ HomeWorkFour/TaskTwo/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача №2: 
+
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+    452 -> 11
+    82 -> 10
+    9012 -> 12
+*/
+
+
+int Imput()
+{
+    int number;
+    Console.Write($"Введите любое число: ");
+    while (int.TryParse(Console.ReadLine(), out number) == false)
+    {
+        Console.Write($"Введено не корректное число.\nВведите положительное натураьное  число: ");
+    }
+    return number;
+}
+
+int Сounting(int num)
+{
+    if (num < 0) num = num * -1;
+    int length = (int)Math.Log10(num);
+    int sum = 0;
+
+    for (int i = 0; i <= length; i++)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+int number = Imput();
+Console.WriteLine($"Сумма цифр в числе: {Сounting(number)}");
