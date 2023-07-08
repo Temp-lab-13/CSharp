43b35f6dc9fc45eaf2b57bc26d8f65b0b27f0be4
Temp-lab-13 <tap-kro-tap@protonmail.com>
2023-07-08 19:14:10 +0700

    Complit TaskFive

--- HomeWorkEight/TaskFive/Program.cs
+++ HomeWorkEight/TaskFive/Program.cs
@@ -6,3 +6,144 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+int number = 01;
+
+//Метод рачпечатывает двумерный массив в фрмате "00", "01" и тд.
+void PrintArray(int[,] arrayFinish)
+{
+    for (int i = 0; i < arrayFinish.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayFinish.GetLength(1); j++)
+        {
+            Console.Write($"{string.Format("{0:d2}", arrayFinish[i, j])} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Метод добавляет в двумерный массив число, по переданным координатам.
+//И увеличивает число на +1.
+void NumPlus(int[,] array, int line, int column)
+{
+    //Проверочка на отсутвие по коориданте уже записанного числа.
+    //Рудемент, предыдущего решения.
+    array[line, column] = number;
+    number++;
+
+}
+
+//Метод проверяет полученный массив на наличие в нём "пустых" мест.
+//Пустыми местами выступают "0".
+bool CheckArrayNull(int[,] array, int line, int column)
+{
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            if (array[i, j] == 0) return true;
+        }
+    }
+    return false;
+}
+
+//Метод заполняет массив числами по спирали. Улиткой.
+void FillingArraySnail(int[,] array, int line, int column)
+{
+    //получаем границы полученного массива.
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+    //Сначала, идёт заполнять впрао до упора, и вниз до упора..
+    while (true)
+    {
+        //Старт. Сдесь записывается первое число.
+        //Предварительно проверив место на "пустоту".
+        if (array[line, column] == 0)
+        {
+            NumPlus(array, line, column);
+            continue;
+        }
+        //Сначало идёт проверка на то не выходим ли мы за границу массива, следующим шагом.
+        //После, идёт проверка, есть ли уже записанное (отличное от 0) число на следуещем шаге.
+        else if ((column + 1) < col && array[line, (column + 1)] == 0)
+        {
+            //Если место пусто и оно ещё в пределах массива - "шагаем".
+            column++;
+            //вызываем метод добавления числа, и перезапускаем цикл.
+            NumPlus(array, line, column);
+            continue;
+        }
+        else if ((line + 1) < row && array[(line + 1), column] == 0)
+        {
+            line++;
+            NumPlus(array, line, column);
+            continue;
+        }
+        else { break; }
+    }
+    //Теперь назад и вверх. 
+    //Цикл разбит на двое, что бы при шагах назад, не начали срабатывать первые условия.
+    while (true)
+    {
+        //Всё тоже самое, но шаги теперь в обратном направлении.
+        if ((column - 1) >= (col * 0) && array[line, (column - 1)] == 0)
+        {
+            column--;
+            NumPlus(array, line, column);
+            continue;
+        }
+        else if ((line - 1) >= (row * 0) && array[(line - 1), column] == 0)
+        {
+            line--;
+            NumPlus(array, line, column);
+            continue;
+        }
+        else { break; }
+    }
+    //Так как при первой итерации обоих циклов, заполняется только первый внешний ряд по кругу,
+    //Необходимо его перезапустить, что бы заполнить внутренние, сколько бы их не было.
+    //Для этого, после отработки циклов, отправляем массив на проверку.
+    //Если массив не заполнен, метод вызывает себя заново, и продолжает заполнять массив.
+    if (CheckArrayNull(array, row, col)) FillingArraySnail(array, line, column);
+}
+
+int[,] matrix = new int[4, 4];
+int lineArray = 0;
+int columnArray = 0;
+FillingArraySnail(matrix, lineArray, columnArray);
+
+PrintArray(matrix);
+
+
+
+//На это не смотрите. Остатки не удавшегося эксперемента.
+/*
+while (row > i)
+{
+    if (NumPlus(matrix, j, i)) break;
+    i++;
+}
+
+while (row > j)
+{
+    if (NumPlus(matrix, j, i)) break;
+    j++;
+
+}
+
+while (row * 0 < i)
+{
+    if (NumPlus(matrix, j, i)) break;
+    i--;
+}
+
+while (row * 0 < j)
+{
+    if (NumPlus(matrix, j, i)) break;
+    j--;
+}
+
+
+i += 1;
+j += 1;
+*/
--- Work/Program.cs
+++ Work/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5-> "1, 2, 3, 4, 5"
+// M = 4; N = 8-> "4, 5, 6, 7, 8"
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+/*
+void Print(int minNumber, int maxNumber)
+{
+    if (minNumber <= maxNumber)
+    {
+        Console.Write($"{minNumber} ");
+        Print(minNumber + 1, maxNumber);
+    }
+}
+Print(1, 5);
+*/
+/*
+int Print(int oneNumber, int twoNumber)
+{
+    if (twoNumber == 0) return 1;
+    return oneNumber * Print(oneNumber, twoNumber - 1);
+}
+Console.Write(Print(3, 5));
+*/
